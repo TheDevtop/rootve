de912cb019685cff8fd1a75eacf1c74031695f36
Thijs Haker <theage2001@gmail.com>
2023-11-19 14:11:45 +0100

    Added build keyword to compile alias

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 START=$(pwd)
-alias compile='go120'
+alias compile='go120 build'
 
-# Build rootexec
 cd rootexec/ && compile && echo 'Built: rootexec'
 cd $START
 
-# Build rootd
 cd rootd/ && compile && echo 'Built: rootd'
 cd $START
 
-# Build rootctl
 cd rootctl/ && compile && echo 'Built: rootctl'
 cd $START
 
