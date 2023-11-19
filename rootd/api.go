4a8e8b1e09e2d7c10bc7550b57889b874a236224
Thijs Haker <theage2001@gmail.com>
2023-11-19 13:54:23 +0100

    Wrote server initialisation code

---
+++ rootd/api.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+func apiStart(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func apiStop(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func apiListAll(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func apiListOnline(w http.ResponseWriter, r *http.Request) {
+
+}
--- rootd/rootd.go
+++ rootd/rootd.go
@@ -1,5 +1,76 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+	"net/http"
+	"sync"
 
+	"github.com/TheDevtop/rootve/pkg/libcsrv"
+	"github.com/TheDevtop/rootve/pkg/libve"
+)
+
+// Global lock and table
+var (
+	vtab libcsrv.VeTable
+	lock sync.Mutex
+)
+
+func autoboot() {
+	var err error
+
+	lock.Lock()
+	for key, val := range vtab {
+		if val.Config.Autoboot {
+			if err = val.Exec.Start(); err != nil {
+				log.Printf("Could not autoboot %s: %s\n", key, err)
+			} else {
+				val.State = libcsrv.StateOn
+				log.Printf("Autobooted %s\n", key)
+			}
+		}
+	}
+	lock.Unlock()
+}
+
+func main() {
+	var (
+		err    error
+		socket net.Listener
+		mux    *http.ServeMux
+		mvc    map[string]libve.VirtConfig
+	)
+
+	// Welcome message
+	log.Println("Starting RootVE Server...")
+
+	// Allocate socket
+	if socket, err = net.Listen("unix", libcsrv.SocketPath); err != nil {
+		log.Panicln(err)
+	}
+	log.Printf("Socket endpoint: %s\n", libcsrv.SocketPath)
+
+	// Read /etc/rootve, initialize vtab
+	if mvc, err = libve.ReadConfig(libve.DefaultPath); err != nil {
+		log.Fatalln(err)
+	}
+	vtab = libcsrv.MakeTable(mvc)
+	log.Println("Initialized table and configuration")
+
+	// Autoboot enabled VE's
+	autoboot()
+
+	// Setup HTTP multiplexer
+	mux.HandleFunc(libcsrv.RouteStart, apiStart)
+	mux.HandleFunc(libcsrv.RouteStop, apiStop)
+	mux.HandleFunc(libcsrv.RouteListAll, apiListAll)
+	mux.HandleFunc(libcsrv.RouteListOnline, apiListOnline)
+
+	// Setup signals
+
+	// Serve WebAPI
+	log.Println("Serving API")
+	if err = http.Serve(socket, mux); err != nil {
+		log.Println(err)
+	}
 }
