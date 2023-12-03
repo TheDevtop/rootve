35d0035f0cb59c02fa43d27de49243204c122b56
Thijs Haker <theage2001@gmail.com>
2023-12-03 16:19:16 +0100

    Implemented jmap, needs its own repository

---
+++ pkg/jmap/jmap.go
@@ -0,0 +1,38 @@
+package jmap
+
+import (
+	"encoding/json"
+	"io"
+)
+
+// Map and write from object to writer
+func Mapto[T any](o T, w io.Writer) error {
+	var (
+		buf []byte
+		err error
+	)
+
+	if buf, err = json.Marshal(o); err != nil {
+		return err
+	}
+	if _, err = w.Write(buf); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Map from reader to object
+func Mapfrom[T any](r io.Reader, o *T) (*T, error) {
+	var (
+		buf []byte
+		err error
+	)
+
+	if buf, err = io.ReadAll(r); err != nil {
+		return o, err
+	}
+	if json.Unmarshal(buf, o); err != nil {
+		return o, err
+	}
+	return o, nil
+}
