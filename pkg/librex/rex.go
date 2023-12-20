5c63a62a2357e6df2c647aebcea02c744e046a6d
Thijs Haker <67151465+TheDevtop@users.noreply.github.com>
2023-12-20 23:16:37 +0100

    Fixed extreme SIGKILL

--- pkg/librex/rex.go
+++ pkg/librex/rex.go
@@ -25,7 +25,7 @@ func (rexPtr *Rex) Start() error {
 
 // Stop rootexec instance
 func (rexPtr *Rex) Stop() error {
-	if err := unix.Kill(-rexPtr.proc.Process.Pid, unix.SIGKILL); err != nil {
+	if err := unix.Kill(-rexPtr.proc.Process.Pid, unix.SIGTERM); err != nil {
 		return err
 	}
 	rexPtr.proc.Process.Wait()
